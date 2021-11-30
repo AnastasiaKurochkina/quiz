7d9b2c9d581c1944f9bffeab470b478340818b2f
Костин Алексей Александрович <lex18908@mail.ru>
2021-11-30 15:08:04 +0000

    checkBack

---
+++ backend/src/routes/quiz.routes.js
@@ -0,0 +1,15 @@
+import QuizController from '../controllers/quiz.controller.js';
+import Router from 'express';
+
+const router = Router();
+
+router.post('/quiz/create', QuizController.createQuiz);
+router.get('/quiz/:id', QuizController.getQuiz);
+
+router.get('/myquiz', QuizController.getAllQuiz);
+router.get('/myquiz/:id', QuizController.getQuiz);
+router.put('/myquiz/edit/:id', QuizController.updateQuiz);
+router.delete('/myquiz/delete/:id', QuizController.deleteQuiz);
+
+const QuizRoutes = router;
+export default QuizRoutes;
